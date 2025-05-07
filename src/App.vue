30b31c21f6ea2e77b8b7bf079a1b2704e3638a40
Eleazar506 <eleazarjarquincv@gmail.com>
2025-05-06 22:00:14 -0600

    SCUNSIS

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Logo from './components/Logo.vue';
+  export default{
+    components:{
+      Logo
+    }
+  }
+
+</script>
+
+<template>
+  <form class="login" id="loginsc">
+    <Logo />
+    <input type="text" class="inputs-txt sub-log" placeholder="Usuario" />
+    <input type="password" class="inputs-txt sub-log" placeholder="Contraseña" />
+    <button type="submit" id="btns" class="submits">Iniciar sesión</button>
+  </form>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="logo">
+      <img src="/public/img/image copy.png" alt="Logo" />
+      SCUNSIS
+    </div>
+  </template>
+  
+  <style scoped>
+  #logo {
+    text-align: center;
+    color: #7C0A02;
+    font-weight: 600;
+    font-size: xx-large;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    top: -30px;
+
+  }
+  #logo img {
+    width: 12vh;
+    margin-bottom: 10px;
+  }
+</style>
+
